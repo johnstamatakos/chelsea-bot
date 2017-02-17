b4ddd9f6dfb203863b6e9297442c83cc2aac92ba
johnstamatakos <jstamatakos@xogrp.com>
2017-02-17 15:45:02 -0600

    Initial commit

---
+++ test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var Botkit = require('botkit');
+
+if (!process.env.SLACK_TOKEN) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var controller = Botkit.slackbot({
+    debug: false
+});
+
+controller.spawn({
+    token: process.env.SLACK_TOKEN
+}).startRTM(function(err, bot, payload) {
+    if (err) {
+        throw new Error(err);
+    }
+});
+
+controller.hears(['Hey Chelsea'], ['ambient'], function(bot, message) {
+    bot.reply(message, 'I never loved Chad - I miss you Mario.');
+});
